8ea24f8dcd9fa06de656a11e33c1d3897c7b925c
CassioUrtiga <cassiourtiga190@gmail.com>
2023-04-01 16:00:12 -0300

    Melhorar segurança

--- salon/settings.py
+++ salon/settings.py
@@ -1,9 +1,10 @@
 from pathlib import Path
+from decouple import config
 import os
 import json
 
 # Abre o arquivo de configurações do banco de dados
-with open('../db_config.json', 'r') as config_file:
+with open('./db_config.json', 'r') as config_file:
     db_config = json.load(config_file)
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -14,10 +15,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY')
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DEBUG', cast=bool)
 
 ALLOWED_HOSTS = ['*']
 
