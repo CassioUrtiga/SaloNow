eea7b6546ae3c84624eefde65af20fff66f69a8b
CassioUrtiga <cassiourtiga190@gmail.com>
2023-04-10 14:22:26 -0300

    Commit inicial

--- core/urls.py
+++ core/urls.py
@@ -1,5 +1,9 @@
 from django.urls import path
 
+from .views import tela_inicial, login, cadastrar
+
 urlpatterns = [
-    
+    path('', tela_inicial, name='inicio'),
+    path('login/', login, name='login'),
+    path('cadastrar/', cadastrar, name='cadastrar')
 ]
--- core/views.py
+++ core/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 # Create your views here.
+def tela_inicial(request):
+    return render(request, 'page_inicial/pg_inicial.html')
+
+def login(request):
+    return render(request, 'page_login/pg_login.html')
+
+def cadastrar(request):
+    return render(request, 'page_login/pg_cadastro.html')
+
