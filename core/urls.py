6f5c1b7080e30d8faff7da57650df849a775c3d6
CarlosVinicius15 <carlosviniciusagripino.sb@gmail.com>
2023-04-14 18:50:30 -0300

    implementação de um dropdown e configurações de rotas

--- core/urls.py
+++ core/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from .views import tela_inicial, login, cadastrar
+from .views import tela_inicial, login, cadastrar_cliente,cadastrar_proprietario
 
 urlpatterns = [
     path('', tela_inicial, name='inicio'),
     path('login/', login, name='login'),
-    path('cadastrar/', cadastrar, name='cadastrar')
+    path('cadastrar-cliente/', cadastrar_cliente, name= 'cadastrar-cliente'),
+    path('cadastrar-proprietario/', cadastrar_proprietario, name= 'cadastrar-proprietario'),
 ]
--- core/views.py
+++ core/views.py
@@ -6,15 +6,18 @@ def tela_inicial(request):
     return render(request, 'page_inicial/pg_inicial.html')
 
 def login(request):
-    return render(request, 'page_login/pg_login.html')
+    return render(request, 'page_login/login.html')
 
-def cadastrar(request):
-    form_proprietario = FormularioProprietario(request.POST)
+def cadastrar_cliente(request):
     form_cliente = FormularioCliente(request.POST)
-
     context = {
-        'form_cliente': form_cliente, 
-        'form_proprietario': form_proprietario
+        'form': form_cliente
     }
-    return render(request, 'page_login/pg_cadastro.html',context)
+    return render(request, 'page_login/cadastro_cliente.html',context)
 
+def cadastrar_proprietario(request):
+    form_proprietario = FormularioProprietario(request.POST)
+    context = {
+        'form': form_proprietario
+    }
+    return render(request, 'page_login/cadastro_proprietario.html',context)
